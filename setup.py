9377a031f1f30ac45cd74046fcda04863a6e3718
Yuval Adam <_@yuv.al>
2018-02-27 00:45:20 +0200

    Fix and release v0.1.1

--- setup.py
+++ setup.py
@@ -7,10 +7,21 @@ with open(str(Path(__file__).resolve().parents[0] / 'README.rst'), encoding='utf
 
 setup(
     name='viewstate',
-    version='0.1.0',
+    version='0.1.1',
+    author='Yuval Adam',
+    author_email='_@yuv.al',
     description='.NET viewstate decoder',
     long_description=long_description,
-    homepage='https://github.com/yuvadm/viewstate',
-    author='Yuval Adam',
+    url='https://github.com/yuvadm/viewstate',
+    license='MIT',
+    python_requires='>=3.5.0',
     packages=find_packages(exclude=['docs', 'tests']),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
 )
